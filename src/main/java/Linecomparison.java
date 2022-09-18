471e0d9b368e31ba25d38c3cd507bd964ed620f3
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-09-18 01:31:21 -0700

    Added: Equality Line

--- src/main/java/Linecomparison.java
+++ src/main/java/Linecomparison.java
@@ -2,25 +2,31 @@ import java.util.Scanner;
 public class Linecomparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comaprison");
-        int x1,x2,y1,y2;
         int a1,a2,b1,b2;
-        double distance ;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value x1=");
-        x1 = sc.nextInt();
-        a1 = sc.nextInt();
-        System.out.println("Enter the value x2=");
-        x2 = sc.nextInt();
-        a2 = sc.nextInt();
-        System.out.println("Enter the value y1=");
-        y1 = sc.nextInt();
-        b1 = sc.nextInt();
-        System.out.println("Enter the value y2=");
-        y2 = sc.nextInt();
-        distance = Math.sqrt(((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
-        b2 = sc .nextInt();
-        distance=Math.sqrt((a2-a1)*(a2-a1)+b2-b1)*(b2-b1);
-        System.out.println("The distance between two lines is" +distance);
+        int a3,a4,b3,b4;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value of x1");
+        a1=sc.nextInt();
+        System.out.println("Enter value of y1");
+        b1=sc.nextInt();
+        System.out.println("Enter value of x2");
+        a2=sc.nextInt();
+        System.out.println("Enter value of y2");
+        b2=sc.nextInt();
+        Double length1=Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
+        System.out.println("The length of line1 is" +length1);
+        System.out.println("Enter value of x3");
+        a3=sc.nextInt();
+        System.out.println("Enter value of y3");
+        b3=sc.nextInt();
+        System.out.println("Enter value of x4");
+        a4=sc.nextInt();
+        System.out.println("Enter value of y4");
+        b4=sc.nextInt();
+        Double length2=Math.sqrt((a4-a3)*(a4-a3)+(b4-b3)*(b4-b3));
+        System.out.println("The length of line2 is" +length2);
+
+        System.out.println(length1 + ".compareTo " +length2+ ":" +length1.compareTo(length2));
 
     }
 }
