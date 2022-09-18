12f27ecb9c88791e35aa3ef9d864254496ba19bf
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-09-18 01:35:12 -0700

    Added: Compare Line

--- src/main/java/Linecomparison.java
+++ src/main/java/Linecomparison.java
@@ -1,32 +1,34 @@
 import java.util.Scanner;
 public class Linecomparison {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line Comaprison");
-        int a1,a2,b1,b2;
-        int a3,a4,b3,b4;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter value of x1");
-        a1=sc.nextInt();
-        System.out.println("Enter value of y1");
-        b1=sc.nextInt();
-        System.out.println("Enter value of x2");
-        a2=sc.nextInt();
-        System.out.println("Enter value of y2");
-        b2=sc.nextInt();
-        Double length1=Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
-        System.out.println("The length of line1 is" +length1);
-        System.out.println("Enter value of x3");
-        a3=sc.nextInt();
-        System.out.println("Enter value of y3");
-        b3=sc.nextInt();
-        System.out.println("Enter value of x4");
-        a4=sc.nextInt();
-        System.out.println("Enter value of y4");
-        b4=sc.nextInt();
-        Double length2=Math.sqrt((a4-a3)*(a4-a3)+(b4-b3)*(b4-b3));
-        System.out.println("The length of line2 is" +length2);
+        public  double Line1 (int x1 ,int x2, int y1 ,int y2) {
+            double length ;
+            double length2;
+            double P = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
+            double length1 = Math.sqrt(P);
+            System.out.println(length1);
+            return length1;
+        }
 
-        System.out.println(length1 + ".compareTo " +length2+ ":" +length1.compareTo(length2));
+        public double Line2 (int x3,int x4,int y3,int y4) {
+            double M = (x3-x4)*(x3-x4) + (y3-y4)*(y3-y4);
+            double length2 = Math.sqrt(M);
+            System.out.println(length2);
+            return length2;
+        }
+        public void  Check ( double length1 , double length2) {
 
+            if (length1 == length2)
+                System.out.println("length are equql");
+            else if (length1 >length2)
+                System.out.println("length1 is greter than length2");
+            else
+                System.out.println("length1 is less than length2");
+        }
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            Linecomparison L1 = new Linecomparison();
+            double length1=L1.Line1(0,3,4,0);
+            double length2=L1.Line2(0,4,4,0);
+            L1.Check(length1, length2);
     }
 }
